f784f68d46275b9452a04ffee114569b787f44d3
jffltt <18552546917@163.com>
2022-07-13 21:26:33 +0800

    20220713

---
+++ src/popup/components/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="popup-header">
+    <el-form :mode="test">
+      <el-form-item label="姓名" label-width="120">
+        <el-input v-model="test.name"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      test: {}
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.popup-header {
+  width: 600px;
+  height: 400px;
+  color: red;
+  &::-webkit-scrollbar{
+    display: none;
+  }
+}
+</style>
